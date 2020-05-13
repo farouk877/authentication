26a9422301cc00651ec8d86167ea574578016047
farouk877 <faroukmarhaba1@gmail.com>
2020-05-13 02:08:44 -0400

    Updates

---
+++ precache-manifest.d14c9d073d336866bac2cd671b2b19fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73acb1511af7ffb84b002d31c39a3c0a",
+    "url": "/authentication/index.html"
+  },
+  {
+    "revision": "7f45ace36e669f24665b",
+    "url": "/authentication/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5b6a221f5137b14f52b",
+    "url": "/authentication/static/js/2.9d6e9d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/authentication/static/js/2.9d6e9d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f45ace36e669f24665b",
+    "url": "/authentication/static/js/main.14e55c44.chunk.js"
+  },
+  {
+    "revision": "465ee49e74676930625a",
+    "url": "/authentication/static/js/runtime-main.b8a0e32c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/authentication/static/media/logo.5d5d9eef.svg"
+  }
+]);
